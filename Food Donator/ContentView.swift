71bd3579bb3724f727d841c3191bc899d8d3ed6e
leeshengyang <leeshengyang@hotmail.com>
2022-06-21 14:59:53 +1000

    tabs

--- Food Donator/ContentView.swift
+++ Food Donator/ContentView.swift
@@ -1,22 +1,35 @@
-//
-//  ContentView.swift
-//  Food Donator
-//
-//  Created by UTS on 21/6/2022.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TabView {
+                Text("Home Tab")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
+                    }
+                Text("Offers Tab 2")
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .tabItem {
+                        Image(systemName: "pencil.slash")
+                        Text("Offers")
+                    }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone 13")
     }
 }
